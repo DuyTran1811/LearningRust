00dff2dadd55409a754c4ed57f56e1814dacebf8
DuyTC <duytc@DuyTCs-MacBook-Pro.local>
2021-12-18 14:08:25 +0700

    Match Control Flow

---
+++ match_controll/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+
+    let five = Some(5);
+    let _six = plus_one(five);
+    let _one = plus_one(None);
+}
